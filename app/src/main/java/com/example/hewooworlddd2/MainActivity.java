ccaf906230cebfad78bec68e7c093c7d6a404f60
Charles C <71957934+Cattosan@users.noreply.github.com>
2020-10-15 23:45:22 +0700

    ini update login :V

--- app/src/main/java/com/example/hewooworlddd2/MainActivity.java
+++ app/src/main/java/com/example/hewooworlddd2/MainActivity.java
@@ -1,20 +1,49 @@
 package com.example.hewooworlddd2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MyActivitiezz";
+    private Button btn;
+    private EditText areaus;
+    private EditText areapazz;
+    private String users = "admin";
+    private String pazz = "admin";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("Berhasil", "In already");
         setContentView(R.layout.activity_main);
-
+        areaus = findViewById(R.id.username);
+        areapazz = findViewById(R.id.password);
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validData();
+            }
+        });
 
         Log.d(TAG, "onCreate: Message nya apa bebas sih, ini updatenya");
         //Log.i(TAG, "onCreate: ");
+    }
+
+    public void validData(){
+        if(areaus.getText().toString().equals(users) && areapazz.getText().toString().equals(pazz)){
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Username atau Pass anda salah", Toast.LENGTH_LONG).show();
+        }
     }
 }
