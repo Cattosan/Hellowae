3aac154149b69ff555cbf776966efa674fd7710a
Charles C <71957934+Cattosan@users.noreply.github.com>
2020-11-12 21:11:44 +0700

    ini update session :V

---
+++ app/src/main/java/com/example/hewooworlddd2/SharedPref.java
@@ -0,0 +1,46 @@
+package com.example.hewooworlddd2;
+/*
+public class SharedPref {
+    public static final String app = "myApp";
+
+    public static final String name = "spNama";
+    public static final String email = "spEmail";
+
+    public static final String isLogin = "loggedIn";
+
+    SharedPref sp;
+    SharedPref.Editor spEditor;
+
+    public SharedPref(Context context){
+        sp = context.getSharedPreferences(app, Context.MODE_PRIVATE);
+        spEditor = sp.edit();
+    }
+
+    public void savespStr(String keySP, String value){
+        spEditor.putString(keySP, value);
+        spEditor.commit();
+    }
+
+    public void savespInt(String keySP, int value){
+        spEditor.putInt(keySP, value);
+        spEditor.commit();
+    }
+
+    public void savespBool(String keySP, boolean value){
+        spEditor.putBoolean(keySP, value);
+        spEditor.commit();
+    }
+
+    public String getName(){
+        return sp.getString(name, "");
+    }
+
+    public String getEmail(){
+        return sp.getString(email, "");
+    }
+
+    public Boolean getLog(){
+        return sp.getBoolean(isLogin, false);
+    }
+}
+*/
